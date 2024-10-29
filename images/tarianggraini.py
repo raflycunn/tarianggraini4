7b6cc6743933d74ebc4bff4873842e2c6560e3f1
raflycunn <raflyprasetia4@gmail.com>
2024-10-29 13:02:50 +0700

    Add files via upload

---
+++ images/tarianggraini.py
@@ -0,0 +1,5 @@
+print("kalkulator luas persegi panjang")
+panjang = input("masukkan panjang :")
+lebar = input("masukkan lebar :")
+luas = int(panjang) * int(lebar)
+print("luas persegi adalah", luas)
---
+++ images/tarii.py
@@ -0,0 +1,4 @@
+print(True)
+print("kami adalah si a1")
+print(42)
+print(2.8999999)
